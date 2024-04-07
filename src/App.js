6e8cd0b2bdab3d66b198f4b08f644d01f64b92e7
unknown <2111cs010308@mallareddyuniversity.ac.in>
2024-04-07 19:23:05 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>
+        welcome to deployment of raectjs
+      </h1>
+    </>
+  );
+}
+
+export default App;
